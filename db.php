168eccc18992ee4c528902eab32930de835b3b1e
khaderhan <khaderhan@gmail.com>
2022-04-16 13:42:07 +0300

    Improved the error handling

--- db.php
+++ db.php
@@ -32,35 +32,34 @@ class db
     public function query($query) {
         $this->closeOpenQuery();
 
-        if ($this->query = $this->connection->prepare($query)) {
-            if (func_num_args() > 1) {
-                $x = func_get_args();
-                $args = array_slice($x, 1);
-                $types = '';
-                $args_ref = array();
-                foreach ($args as $k => &$arg) {
-                    if (is_array($args[$k])) {
-                        foreach ($args[$k] as $j => &$a) {
-                            $types .= $this->_gettype($args[$k][$j]);
-                            $args_ref[] = &$a;
-                        }
-                    } else {
-                        $types .= $this->_gettype($args[$k]);
-                        $args_ref[] = &$arg;
-                    }
-                }
-                array_unshift($args_ref, $types);
-                call_user_func_array(array($this->query, 'bind_param'), $args_ref);
-            }
-            $this->query->execute();
-            if ($this->query->errno) {
-                $this->error('Unable to process MySQL query (check your params) - ' . $this->query->error);
-            }
-            $this->query_closed = FALSE;
-            $this->query_count++;
-        } else {
+        if (!$this->query = $this->connection->prepare($query)) {
             $this->error('Unable to prepare MySQL statement (check your syntax) - ' . $this->connection->error);
         }
+        if (func_num_args() > 1) {
+            $x = func_get_args();
+            $args = array_slice($x, 1);
+            $types = '';
+            $args_ref = array();
+            foreach ($args as $k => &$arg) {
+                if (is_array($args[$k])) {
+                    foreach ($args[$k] as $j => &$a) {
+                        $types .= $this->_gettype($args[$k][$j]);
+                        $args_ref[] = &$a;
+                    }
+                } else {
+                    $types .= $this->_gettype($args[$k]);
+                    $args_ref[] = &$arg;
+                }
+            }
+            array_unshift($args_ref, $types);
+            call_user_func_array(array($this->query, 'bind_param'), $args_ref);
+        }
+        $this->query->execute();
+        if ($this->query->errno) {
+            $this->error('Unable to process MySQL query (check your params) - ' . $this->query->error);
+        }
+        $this->query_closed = FALSE;
+        $this->query_count++;
         return $this;
     }
 
